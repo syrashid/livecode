dd2a0d6f34af781d8711f92d6d73907561e6c38c
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 09:39:25 +1000

    livecode for cookbook

---
+++ OOP/cookbook-reboot/lib/view.rb
@@ -0,0 +1,15 @@
+class View
+  def display(recipes)
+    puts "Listing all of your recipes!"
+    puts " -------------------------- "
+    recipes.each_with_index do |recipe, index|
+      puts "#{index + 1}. #{recipe.name}: #{recipe.description}"
+    end
+  end
+
+  def ask_for(element)
+    puts "Please provide a recipe #{element}"
+    print "> "
+    gets.chomp
+  end
+end
