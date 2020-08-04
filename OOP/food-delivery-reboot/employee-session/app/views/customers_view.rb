95c15914c199a3a2351590cfd258b8c5220dcdfe
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-04 22:04:23 +1000

    Added employee model and repo

---
+++ OOP/food-delivery-reboot/employee-session/app/views/customers_view.rb
@@ -0,0 +1,17 @@
+class CustomersView
+  def display(customers)
+    customers.each_with_index do |customer, index|
+      # We use to_s as a clean way for the element
+      # in question to present it's data
+      puts "#{index + 1}. #{customer}"
+    end
+  end
+
+  # Generic ask method for information from the user
+  # Needs to be passed a string, output is a string
+  def ask_user_for(stuff)
+    puts "#{stuff.capitalize} of the customer?"
+    print "> "
+    return gets.chomp
+  end
+end
