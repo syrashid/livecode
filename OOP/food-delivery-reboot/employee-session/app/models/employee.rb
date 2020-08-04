9ecf5c3609c9625e6bdc7e74d7d9777a1b1bf80b
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-04 22:32:48 +1000

    model fixes

--- OOP/food-delivery-reboot/employee-session/app/models/employee.rb
+++ OOP/food-delivery-reboot/employee-session/app/models/employee.rb
@@ -2,6 +2,10 @@ class Employee
   attr_accessor :id
   attr_reader :username, :password, :role
 
+  def self.csv_headers
+    %w[id name password role]
+  end
+
   def initialize(attributes = {})
     @id = attributes[:id]
     @username = attributes[:username]
@@ -15,5 +19,9 @@ class Employee
 
   def delivery_guy?
     @role == "delivery_guy"
+  end
+
+  def to_array
+    [@id, @name, @password, @role]
   end
 end
