0d7cdfe6c488665624e42f5901d3c9830b537b9f
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-04 22:25:54 +1000

    livecode complete

--- OOP/food-delivery-reboot/employee-session/router.rb
+++ OOP/food-delivery-reboot/employee-session/router.rb
@@ -4,59 +4,96 @@ class Router
     # so that I can use them throughout the Router
     @meals_controller = attr[:meals_controller]
     @customers_controller = attr[:customers_controller]
+    @sessions_controller = attr[:sessions_controller]
     @running = true
   end
 
-  # This method kicks off the actual program
   def run
     puts "Welcome to the Le Wagon Pub!"
     puts "----------------------------"
-
-    # After an action is completed, I want to
-    # display the menu again (looping)
-    # This should only stop when running is false
     while @running
-      # Print all user action options
-      print_menu
-      # Retrieve the choice, convert to integer
-      choice = gets.chomp.to_i
+      # First step is to check weather or not the current
+      # user is an employee of the restaurant
+      @current_user = @sessions_controller.login
+      while @current_user
+        if @current_user.manager?
+          route_manager_action
+        else
+          route_delivery_guy_action
+        end
+      end
       print `clear`
-      # Route the user desire to the correct controller action
-      route_action(choice)
     end
   end
 
   private
 
-  def print_menu
+  def route_manager_action
+    print_manager_menu
+    choice = gets.chomp.to_i
+    print `clear`
+    manager_action(choice)
+  end
+
+  def route_delivery_guy_action
+    print_delivery_guy_menu
+    choice = gets.chomp.to_i
+    print `clear`
+    delivery_guy_action(choice)
+  end
+
+  def print_manager_menu
     puts "--------------------"
-    puts "------- MENU -------"
+    puts "---- MGR MENU 🧔 ---"
     puts "--------------------"
     puts "1. Add new meal"
     puts "2. List all meals"
     puts "3. Add new customer"
     puts "4. List all customers"
-    puts "8. Exit"
+    puts "5. Logout"
+    puts "6. Exit"
     print "> "
   end
 
-  # Dispatch the User REQUEST to the
-  # SPECIFIC controller action/method
-  # that implements what the User wants to do
-  def route_action(choice)
+  def print_delivery_guy_menu
+    puts "--------------------"
+    puts "---- EMP MENU 👦 ---"
+    puts "--------------------"
+    puts "1. Delivery guy action #1"
+    puts "2. Delivery guy action #2"
+    puts "3. Logout"
+    puts "4. Exit"
+    print "> "
+  end
+
+  def manager_action(choice)
     case choice
     when 1 then @meals_controller.add
     when 2 then @meals_controller.list
     when 3 then @customers_controller.add
     when 4 then @customers_controller.list
-    when 8 then stop!
+    when 5 then logout!
+    when 6 then stop!
     else puts "Try again..."
     end
   end
 
-  # Method to stop the code running by changing
-  # the running state
+  def delivery_guy_action(choice)
+    case choice
+    when 1 then puts "Complete delivery action 1"
+    when 2 then puts "Complete delivery action 2"
+    when 3 then logout!
+    when 4 then stop!
+    else puts "Try again..."
+    end
+  end
+
+  def logout!
+    @current_user = nil
+  end
+
   def stop!
+    logout!
     @running = false
   end
 end
