5c807443ae0552dc7eae91f5b86e5a319ab2f99f
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-04 22:41:33 +1000

    lecture prep

--- OOP/food-delivery-reboot/employee-session/app/controllers/sessions_controller.rb
+++ OOP/food-delivery-reboot/employee-session/app/controllers/sessions_controller.rb
@@ -11,6 +11,9 @@ class SessionsController
     password = @sessions_view.ask_for(:password)
     employee = @employee_repository.find_by_username(username)
     return employee if employee && employee.password == password
+    # Stop the code here, code it for only one loop, then work through the process of looping it in the correct manner
+
+    # After you show it work once, then do the below
 
     @sessions_view.print_wrong_credentials
     login
