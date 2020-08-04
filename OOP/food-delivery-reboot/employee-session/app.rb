260631a5f59a831f9884cf6389cbd18d054024b9
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-04 22:13:19 +1000

    added sessions controller

--- OOP/food-delivery-reboot/employee-session/app.rb
+++ OOP/food-delivery-reboot/employee-session/app.rb
@@ -1,14 +1,17 @@
 # Require all models
 require_relative 'app/models/meal'
 require_relative 'app/models/customer'
+require_relative 'app/models/employee'
 
 # Require all repositories
 require_relative 'app/repositories/meal_repository'
 require_relative 'app/repositories/customer_repository'
+require_relative 'app/repositories/employee_repository'
 
 # Require all controllers
 require_relative 'app/controllers/meals_controller'
 require_relative 'app/controllers/customers_controller'
+require_relative 'app/controllers/sessions_controller'
 
 # Require the router
 require_relative "router"
@@ -32,10 +35,19 @@ customer_repository = CustomerRepository.new(CUSTOMERS_CSV_FILE)
 # that can be used to create the CustomersController:
 customers_controller = CustomersController.new(customer_repository)
 
+# In order to create a SessionsController, I need to...
+# have a string with the Employees CSV filepath...
+EMPLOYEES_CSV_FILE = File.join(__dir__, "data/employees.csv")
+# so that I can use it to create a EmployeeRepository...
+employee_repository = EmployeeRepository.new(EMPLOYEES_CSV_FILE)
+# that can be used to create the CustomersController:
+sessions_controller = SessionsController.new(employee_repository)
+
 # Organize all controllers in a hash to be used in the Router:
 controllers = {
   customers_controller: customers_controller,
-  meals_controller: meals_controller
+  meals_controller: meals_controller,
+  sessions_controller: sessions_controller
 }
 
 router = Router.new(controllers)
