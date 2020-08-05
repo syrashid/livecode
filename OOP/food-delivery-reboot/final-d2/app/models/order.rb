24ad4329785aa4054c4b713d866e68117666e612
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-05 13:27:05 +1000

    set up order class

---
+++ OOP/food-delivery-reboot/final-d2/app/models/order.rb
@@ -0,0 +1,28 @@
+class Order
+  attr_accessor :id, :employee
+  attr_reader :meal, :customer, :delivered
+
+  def self.csv_headers
+    %w[id delivered meal_id customer_id employee_id]
+  end
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @meal = attributes[:meal]
+    @customer = attributes[:customer]
+    @employee = attributes[:employee]
+    @delivered = attributes[:delivered] || false
+  end
+
+  def delivered?
+    @delivered
+  end
+
+  def deliver!
+    @delivered = true
+  end
+
+  def to_array
+    [@id, @delivered, @meal.id, @customer.id, employee.id]
+  end
+end
