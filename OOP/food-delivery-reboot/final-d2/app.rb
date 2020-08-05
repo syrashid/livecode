3ec05b9f02174eaedb3382ba466d5a72537a1800
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-05 21:34:20 +1000

    comments for app file

--- OOP/food-delivery-reboot/final-d2/app.rb
+++ OOP/food-delivery-reboot/final-d2/app.rb
@@ -1,8 +1,3 @@
-# Require all models
-require_relative 'app/models/meal'
-require_relative 'app/models/customer'
-require_relative 'app/models/employee'
-
 # Require all repositories
 require_relative 'app/repositories/meal_repository'
 require_relative 'app/repositories/customer_repository'
@@ -45,13 +40,7 @@ employee_repository = EmployeeRepository.new(EMPLOYEES_CSV_FILE)
 # that can be used to create the CustomersController:
 sessions_controller = SessionsController.new(employee_repository)
 
-# Organize all controllers in a hash to be used in the Router:
-controllers = {
-  customers_controller: customers_controller,
-  meals_controller: meals_controller,
-  sessions_controller: sessions_controller
-}
-
+# As the order repo needs access to all of the repositories we package it into a single repos hash we can use again for the controller
 repositories = {
   customer_repo: customer_repository,
   meal_repo: meal_repository,
@@ -68,8 +57,13 @@ repositories[:order_repo] = order_repository
 
 orders_controller = OrdersController.new(repositories)
 
-controllers[:orders_controller] = orders_controller
-
+# Organize all controllers in a hash to be used in the Router:
+controllers = {
+  customers_controller: customers_controller,
+  meals_controller: meals_controller,
+  sessions_controller: sessions_controller,
+  orders_controller: orders_controller
+}
 
 router = Router.new(controllers)
 
