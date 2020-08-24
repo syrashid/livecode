b3fe8c71e1446f7e0c972815bcf184046cd0d7c1
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-24 15:27:42 +1000

    FIX: BBCGoodFood Scraper

--- OOP/cookbook-d2-reboot/final/lib/services/scrapebbcgoodfood.rb
+++ OOP/cookbook-d2-reboot/final/lib/services/scrapebbcgoodfood.rb
@@ -1,17 +1,33 @@
 require 'nokogiri'
-require 'open-uri'
+require 'watir'
 require_relative '../recipe'
 
 class ScrapeBBCGoodFood
   def self.call(keyword)
-    html = open("https://www.bbcgoodfood.com/search/recipes?query=#{keyword}").read
-    doc = Nokogiri::HTML(html, nil, "utf-8")
-    doc.search(".node-recipe").first(5).collect do |node|
+    # Set up watir, wait until page contents that are needed are fully loaded
+
+    # Leave the below line as is if you don't want a browser window to open
+    browser = Watir::Browser.new :chrome, headless: true
+    # Comment in the below line, and comment out the above line if you want to see a browser window pop up
+    # browser = Watir::Browser.new
+    browser.goto "https://www.bbcgoodfood.com/search/recipes?q=#{keyword}"  # The url has changed slightly
+
+    # This line is important because it is waiting for the elements we need to load before translating the html
+    browser.element(class: "standard-card-new__additional-info-item").wait_until(&:present?)
+
+    # Translate html into nokogiri doc
+    doc = Nokogiri::HTML.parse(browser.html, nil, "utf-8")
+
+    # Comment in the below line to close the browser window once the page contents have loaded
+    # browser.close
+
+    # The classes in bbcgoodfood have changed as well for the different elements
+    doc.search(".standard-card-new").first(5).collect do |node|
       attr = {}
-      attr[:name] = node.search(".teaser-item__title").text.strip
-      attr[:description] = node.search(".field-item").text.strip
-      attr[:prep_time] = node.search(".teaser-item__info-item--total-time").text.strip
-      attr[:difficulty] = node.search(".teaser-item__info-item--skill-level").text.strip
+      attr[:name] = node.search(".standard-card-new__article-title").text.strip
+      attr[:description] = node.search(".standard-card-new__description").text.strip
+      attr[:prep_time] = node.search(".standard-card-new__additional-info-item")[0].text.strip
+      attr[:difficulty] = node.search(".standard-card-new__additional-info-item")[1].text.strip
       Recipe.new(attr)
     end
   end
