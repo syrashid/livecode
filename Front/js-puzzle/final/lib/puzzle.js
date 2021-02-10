ad433a1837c2d87237ed2b433d85042fdf7a04e0
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-09 21:46:29 -0500

    FEAT: Added hilarious paal images and styling

--- Front/js-puzzle/final/lib/puzzle.js
+++ Front/js-puzzle/final/lib/puzzle.js
@@ -1,1 +1,11 @@
-// todo
+// Decorate
+(() => {
+  const pieces = document.querySelectorAll('td');
+  pieces.forEach((piece) => {
+    if (piece.dataset.number) piece.style.backgroundImage = `url(./images/${piece.dataset.number}.jpg)`;
+
+  })
+})();
+
+
+// DO NOT TOUCH CODE ABOVE 👆
