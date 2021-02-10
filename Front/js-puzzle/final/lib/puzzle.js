f53bb83fd15f4fd9e69b80b4fce787d59776709f
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-09 21:59:36 -0500

    FEAT: Abstracted code into key four steps

--- Front/js-puzzle/final/lib/puzzle.js
+++ Front/js-puzzle/final/lib/puzzle.js
@@ -3,7 +3,6 @@
   const pieces = document.querySelectorAll('td');
   pieces.forEach((piece) => {
     if (piece.dataset.number) piece.style.backgroundImage = `url(./images/${piece.dataset.number}.jpg)`;
-
   })
 })();
 
@@ -14,3 +13,27 @@ const hintBtn = document.getElementById('show-hint');
 const hint = document.querySelector('.hint');
 
 document.addEventListener('click', () => hint.classList.toggle('active'));
+
+// Logic for checking empty spots
+// 4. If it has an empty neighbor
+const canMove = (tile) => {
+  console.log(tile);
+}
+
+// Logic for tile swapping
+// 5. Swap the tile and the empty space
+// 6. Check if player wins
+const tileSwap = () => {
+  canMove(event.currentTarget);
+  console.log("I Was clicked");
+};
+
+// 1. Select all tiles
+const tiles = document.querySelectorAll("td");
+
+// 2. For each tile
+// 3. Listen to the click event
+tiles.forEach((tile) => tile.addEventListener('click', tileSwap))
+
+
+
