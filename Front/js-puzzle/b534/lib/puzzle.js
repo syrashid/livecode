ad8d3796590b361ba943428b9e0711455631fb5f
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-11 01:01:53 -0500

    FIX: Cleaning up spacing

--- Front/js-puzzle/b534/lib/puzzle.js
+++ Front/js-puzzle/b534/lib/puzzle.js
@@ -3,9 +3,7 @@
   const pieces = document.querySelectorAll('td');
   pieces.forEach((piece) => {
     if (piece.dataset.number) piece.style.backgroundImage = `url(./images/${piece.dataset.number}.jpg)`;
-
   })
 })();
-
 
 // DO NOT TOUCH CODE ABOVE 👆
